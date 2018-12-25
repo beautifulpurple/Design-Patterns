ee99a31509a165984eff8002fcaf3b440b5b3c09
黄强华 <huangqianghua@outlook.com>
2018-12-25 18:34:18 +0800

    第一次

---
+++ PatternProject/Singleton/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    /// <summary>
+    /// C#中经典的单例模式使用方式
+    /// </summary>
+    class DemoOne
+    {
+        public static readonly DemoOne Instance = new DemoOne();
+
+        private DemoOne()
+        {
+        }
+    }
+    /// <summary>
+    ///上面的代码  相当于下面这种
+    /// </summary>
+    class DemoOneExt
+    {
+        public static readonly DemoOneExt Instance;
+
+        static DemoOneExt()
+        {
+            Instance = new DemoOneExt();
+        }
+
+        private DemoOneExt()
+        {
+        }
+    }
+}
